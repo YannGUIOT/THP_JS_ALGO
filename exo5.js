7fdfb90f4819bd42e6573e68e39c832c271463b6
YannGUIOT <110197955+YannGUIOT@users.noreply.github.com>
2023-01-12 10:04:51 +0100

    Js Algo exercices

--- exo5.js
+++ exo5.js
@@ -13,4 +13,4 @@ let result1 = hasTwoNumbersWithSumK([10, 5, 3, 7],17);
 console.log("List : [10, 5, 3, 7] - k = 17 => " + result1);
 
 let result2 = hasTwoNumbersWithSumK([10, 5, 3, 7],14);
-console.log("List : [10, 5, 3, 7] - k = 17 => " + result2);
+console.log("List : [10, 5, 3, 7] - k = 14 => " + result2);
